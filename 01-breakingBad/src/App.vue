2949fd1b6d503f7848d7c43c54561227b94bea6d
René <diaz.de.sandi.7@gmail.com>
2023-06-14 14:56:24 -0700

    script setup

---
+++ 01-breakingBad/src/App.vue
@@ -0,0 +1,44 @@
+<script lang ="ts" src="./App.ts">
+
+// Al separar el código en un archivo independiente,
+// no se utilizaría setup.
+// import { ref } from 'vue';
+
+// const counter = ref<number>(0);
+
+
+/*
+export default defineComponent({
+  setup() {
+    const counter = ref(0);
+    const increase = () => {
+      counter.value++
+    }
+
+    return {
+      // Properties
+      counter,
+
+      // Methods
+      increase
+    }
+  }
+})
+*/
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Counter {{ counter }} </h1>
+    <button @click="counter++">+1</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  /* display: flex; */
+  place-items: flex-start;
+  flex-wrap: wrap;
+}
+</style>
