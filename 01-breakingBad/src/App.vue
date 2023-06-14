721e69b4811aac8e36ed2b39f3245f0c1410ab68
René <diaz.de.sandi.7@gmail.com>
2023-06-14 15:05:51 -0700

    file structure

--- 01-breakingBad/src/App.vue
+++ 01-breakingBad/src/App.vue
@@ -1,37 +1,10 @@
-<script lang ="ts" src="./App.ts">
-
-// Al separar el código en un archivo independiente,
-// no se utilizaría setup.
-// import { ref } from 'vue';
-
-// const counter = ref<number>(0);
-
-
-/*
-export default defineComponent({
-  setup() {
-    const counter = ref(0);
-    const increase = () => {
-      counter.value++
-    }
-
-    return {
-      // Properties
-      counter,
-
-      // Methods
-      increase
-    }
-  }
-})
-*/
+<script lang ="ts">
 
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>Counter {{ counter }} </h1>
-    <button @click="counter++">+1</button>
+      <h1>App.vue </h1>
   </div>
 </template>
 
