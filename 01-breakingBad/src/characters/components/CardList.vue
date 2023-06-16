4b4e473d2c36c3d6944346b9ef83c6923bd2a346
René <diaz.de.sandi.7@gmail.com>
2023-06-15 17:11:28 -0700

    http requests

---
+++ 01-breakingBad/src/characters/components/CardList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import rickMortyApi from '@/api/rickMortyApi';
+import type { Results } from '../interfaces/character';
+
+
+rickMortyApi.get<Results>('/character').then(resp => {
+    const name = resp.data.results
+    console.log(name)
+})
+
+</script>
+<template>
+    <ul>
+        <li>Hola</li>
+    </ul>
+</template>
+
+
+<style scoped></style>
