24bb4c85dd7de692a024c625c90631cd8b3ad5f3
René <diaz.de.sandi.7@gmail.com>
2023-06-16 18:54:05 -0700

    useQuery

--- 01-breakingBad/src/characters/components/CardList.vue
+++ 01-breakingBad/src/characters/components/CardList.vue
@@ -2,23 +2,54 @@
 // import rickMortyApi from "@/api/rickMortyApi";
 // import type { Character, Results } from "../interfaces/character";
 // import { ref } from "vue";
+import { useQuery } from "@tanstack/vue-query";
 import { useCharacters } from "../composable/useCharacters";
+import rickMortyApi from "@/api/rickMortyApi";
+import type { Character, Results } from "../interfaces/character";
 
+// 1.- Normal Suspense
 // const { data } = await rickMortyApi.get<Results>("/character");
 // const { results } = data;
 // console.log(results);
 
 // const characters = ref<Character[]>(data.results);
 
-const { isLoading, characters, hasError, errorMessage } = useCharacters();
+// 2.- Composable functions
+// const { isLoading, characters, hasError, errorMessage } = useCharacters();
+
+// 3.- TanStack Query
+const getCharactersSlow = async (): Promise<Character[]> => {
+
+    return new Promise((resolve)=> {
+        setTimeout(async()=> {
+            const { data } = await rickMortyApi.get<Results>("/character");
+            resolve(data.results)
+        },3000)
+    })
+
+//   const { data } = await rickMortyApi.get<Results>("/character");
+//   return data.results;
+};
+
+const { isLoading, isError, data, error } = useQuery(
+  // Elemento que se quiere mantener en cache:
+  ["characters"],
+  // Función para obtener los datos
+  getCharactersSlow,
+  // Tiempo de permanencia de la información en la cache.
+  {
+    cacheTime: 1000 * 60,
+    refetchOnReconnect: 'always'
+  }
+);
 </script>
 <template>
-        <h1 v-if="isLoading">Loading...</h1>
-        <h1 v-if="hasError">{{ errorMessage }}</h1>
-        <ul>
-            <li v-for="{ id, name } of characters" :key="id">{{ name }}</li>
-            <!-- <li v-for="{ id, name } of results" :key="id">{{ name }}</li> -->
-    </ul>
+  <h1 v-if="isLoading">Loading...</h1>
+  <h1 v-if="isError">{{ error }}</h1>
+  <ul>
+    <li v-for="{ id, name } of data" :key="id">{{ name }}</li>
+    <!-- <li v-for="{ id, name } of results" :key="id">{{ name }}</li> -->
+  </ul>
 </template>
 
 <style scoped></style>
--- 01-breakingBad/src/main.ts
+++ 01-breakingBad/src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(VueQueryPlugin).use(router).mount("#app");
