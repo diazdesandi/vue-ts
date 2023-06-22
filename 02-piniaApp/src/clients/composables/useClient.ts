ae6039a3a935c584ace83eba7e7829e3f94ecbbd
René <diaz.de.sandi.7@gmail.com>
2023-06-22 11:59:23 -0700

    useMutation

--- 02-piniaApp/src/clients/composables/useClient.ts
+++ 02-piniaApp/src/clients/composables/useClient.ts
@@ -11,12 +11,18 @@ const getClient = async (id: number) => {
 const useCounter = (id: number) => {
   const client = ref<Client>();
 
-  const { isLoading, data } = useQuery(["client", id], () => getClient(id));
+  const { isLoading, data } = useQuery(["client", id], () => getClient(id), {});
 
   watch(
     data,
     () => {
+      /*
+      client.value = data.value asigna la referncia de data a client,
+      como data es readonly, client tambien lo es
       if (data.value) client.value = data.value;
+      */
+      // Usamos desestructuración para cortar la referencia
+      if (data.value) client.value = { ...data.value };
     },
     { immediate: true }
   );
--- 02-piniaApp/src/clients/pages/ClientPage.vue
+++ 02-piniaApp/src/clients/pages/ClientPage.vue
@@ -2,22 +2,53 @@
 import LoadingModal from "@/shared/components/LoadingModal.vue";
 import useClient from "@/clients/composables/useClient";
 import { useRoute } from "vue-router";
+import { useMutation } from "@tanstack/vue-query";
+import type { Client } from "../interfaces/client";
+import clientsApi from "@/api/clients-api";
+import { watch } from "vue";
 
 const route = useRoute();
 
 const { isLoading, client } = useClient(+route.params.id);
+
+// Actualizando cliente
+const updateClient = async (client: Client): Promise<Client> => {
+  await new Promise((resolve) => {
+    setTimeout(() => resolve(true), 1500);
+  });
+
+  const { id, ...rest } = client;
+
+  const { data } = await clientsApi.patch<Client>(`/clients/${id}`, rest);
+  return data;
+};
+
+const clientMutation = useMutation(updateClient);
+
+// Eliminar anuncio "Guardado! y "Guardando..."
+
+watch(clientMutation.isSuccess, () => {
+  setTimeout(() => {
+    clientMutation.reset();
+  }, 2000);
+});
 </script>
 <template>
+  <h3 v-if="clientMutation.isLoading.value">Guardando...</h3>
+  <h3 v-if="clientMutation.isSuccess.value">Guardado!</h3>
+
   <LoadingModal v-if="isLoading" />
 
   <div v-if="client">
     <h1>{{ client.name }}</h1>
-    <form>
-      <input type="text" placeholder="Nombre" :value="client.name" />
+    <form @submit.prevent="clientMutation.mutate(client)">
+      <input type="text" placeholder="Nombre" v-model="client.name" />
       <br />
-      <input type="text" placeholder="Dirección" :value="client.address" />
+      <input type="text" placeholder="Dirección" v-model="client.address" />
       <br />
-      <button type="submit">Guardar</button>
+      <button :disabled="clientMutation.isLoading.value" type="submit">
+        Guardar
+      </button>
     </form>
   </div>
   <code> {{ client }} </code>
